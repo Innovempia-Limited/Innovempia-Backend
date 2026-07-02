a1161d7670dbafc5b66a6367dab128364a6b9d02
Olivia Oguelina <oliviaoguelina@gmail.com>
2026-07-02 01:59:21 +0100

    swagger doc and setup

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({ origin: true, credentials: true });
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Innovempia API')
+    .setDescription('Mentorship platform backend')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: { persistAuthorization: true },
+  });
+
+  await app.listen(3000);
+  console.log('Running at http://localhost:3000/api');
+  console.log('Swagger at http://localhost:3000/api/docs');
+}
+bootstrap();
