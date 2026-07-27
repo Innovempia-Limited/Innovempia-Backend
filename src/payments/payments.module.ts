f60aefe996aec1b24edeeaa69f483cdae8d9fcc4
Olivia Oguelina <oliviaoguelina@gmail.com>
2026-07-27 07:40:02 +0100

    fixed

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 
+import { AuthModule } from '../auth/auth.module';
 import { EmailModule } from '../email/email.module';
 import { PrismaModule } from '../prisma/prisma.module';
 
@@ -7,7 +8,7 @@ import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 
 @Module({
-  imports: [PrismaModule, EmailModule],
+  imports: [PrismaModule, EmailModule, AuthModule],
   controllers: [PaymentsController],
   providers: [PaymentsService],
   exports: [PaymentsService],
